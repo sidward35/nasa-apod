4ed818fcedd6fbb08f83b538dbeb2fccea4bcd47
Siddharth Mathur <sid@stscamps.org>
2017-07-16 17:09:41 -0700

    Add icon, randomize images, add next button

--- app/src/main/java/com/sid/nasaapod/MainActivity.java
+++ app/src/main/java/com/sid/nasaapod/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -39,7 +41,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        //final int[] subtraction = {0};
+        updateImages(/*subtraction[0]*/);
 
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //subtraction[0] +=10;
+                updateImages(/*subtraction[0]*/);
+            }
+        });
+    }
+
+    public void updateImages(){
+        ImageView imageView = (ImageView)findViewById(R.id.imageView), imageView2 = (ImageView)findViewById(R.id.imageView2), imageView3 = (ImageView)findViewById(R.id.imageView3), imageView4 = (ImageView)findViewById(R.id.imageView4), imageView5 = (ImageView)findViewById(R.id.imageView5), imageView6 = (ImageView)findViewById(R.id.imageView6), imageView7 = (ImageView)findViewById(R.id.imageView7), imageView8 = (ImageView)findViewById(R.id.imageView8), imageView9 = (ImageView)findViewById(R.id.imageView9), imageView10 = (ImageView)findViewById(R.id.imageView10);
+        TextView textView = (TextView)findViewById(R.id.textView), textView2 = (TextView)findViewById(R.id.textView2), textView3 = (TextView)findViewById(R.id.textView3), textView4 = (TextView)findViewById(R.id.textView4), textView5 = (TextView)findViewById(R.id.textView5), textView6 = (TextView)findViewById(R.id.textView6), textView7 = (TextView)findViewById(R.id.textView7), textView8 = (TextView)findViewById(R.id.textView8), textView9 = (TextView)findViewById(R.id.textView9), textView10 = (TextView)findViewById(R.id.textView10);
+
+        getCard(imageView, textView, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView2, textView2, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView3, textView3, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView4, textView4, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView5, textView5, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView6, textView6, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView7, textView7, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView8, textView8, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView9, textView9, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        getCard(imageView10, textView10, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+    }
+
+
+    public void updateImages(int sub){
         ImageView imageView = (ImageView)findViewById(R.id.imageView), imageView2 = (ImageView)findViewById(R.id.imageView2), imageView3 = (ImageView)findViewById(R.id.imageView3), imageView4 = (ImageView)findViewById(R.id.imageView4), imageView5 = (ImageView)findViewById(R.id.imageView5), imageView6 = (ImageView)findViewById(R.id.imageView6), imageView7 = (ImageView)findViewById(R.id.imageView7), imageView8 = (ImageView)findViewById(R.id.imageView8), imageView9 = (ImageView)findViewById(R.id.imageView9), imageView10 = (ImageView)findViewById(R.id.imageView10);
         TextView textView = (TextView)findViewById(R.id.textView), textView2 = (TextView)findViewById(R.id.textView2), textView3 = (TextView)findViewById(R.id.textView3), textView4 = (TextView)findViewById(R.id.textView4), textView5 = (TextView)findViewById(R.id.textView5), textView6 = (TextView)findViewById(R.id.textView6), textView7 = (TextView)findViewById(R.id.textView7), textView8 = (TextView)findViewById(R.id.textView8), textView9 = (TextView)findViewById(R.id.textView9), textView10 = (TextView)findViewById(R.id.textView10);
 
@@ -47,19 +79,18 @@ public class MainActivity extends AppCompatActivity {
         Log.e("Current time => ", ""+c.getTime());
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         String formattedDate = df.format(c.getTime());
-
         Log.e("date=", formattedDate);
 
-        getCard(imageView, textView, ""+(Integer.parseInt(formattedDate.substring(0,4))-1)+formattedDate.substring(4));
-        getCard(imageView2, textView2, ""+(Integer.parseInt(formattedDate.substring(0,4))-2)+formattedDate.substring(4));
-        getCard(imageView3, textView3, ""+(Integer.parseInt(formattedDate.substring(0,4))-3)+formattedDate.substring(4));
-        getCard(imageView4, textView4, ""+(Integer.parseInt(formattedDate.substring(0,4))-4)+formattedDate.substring(4));
-        getCard(imageView5, textView5, ""+(Integer.parseInt(formattedDate.substring(0,4))-5)+formattedDate.substring(4));
-        getCard(imageView6, textView6, ""+(Integer.parseInt(formattedDate.substring(0,4))-6)+formattedDate.substring(4));
-        getCard(imageView7, textView7, ""+(Integer.parseInt(formattedDate.substring(0,4))-7)+formattedDate.substring(4));
-        getCard(imageView8, textView8, ""+(Integer.parseInt(formattedDate.substring(0,4))-8)+formattedDate.substring(4));
-        getCard(imageView9, textView9, ""+(Integer.parseInt(formattedDate.substring(0,4))-9)+formattedDate.substring(4));
-        getCard(imageView10, textView10, ""+(Integer.parseInt(formattedDate.substring(0,4))-10)+formattedDate.substring(4));
+        getCard(imageView, textView, ""+(Integer.parseInt(formattedDate.substring(0,4))-(1+sub))+formattedDate.substring(4));
+        getCard(imageView2, textView2, ""+(Integer.parseInt(formattedDate.substring(0,4))-(2+sub))+formattedDate.substring(4));
+        getCard(imageView3, textView3, ""+(Integer.parseInt(formattedDate.substring(0,4))-(3+sub))+formattedDate.substring(4));
+        getCard(imageView4, textView4, ""+(Integer.parseInt(formattedDate.substring(0,4))-(4+sub))+formattedDate.substring(4));
+        getCard(imageView5, textView5, ""+(Integer.parseInt(formattedDate.substring(0,4))-(5+sub))+formattedDate.substring(4));
+        getCard(imageView6, textView6, ""+(Integer.parseInt(formattedDate.substring(0,4))-(6+sub))+formattedDate.substring(4));
+        getCard(imageView7, textView7, ""+(Integer.parseInt(formattedDate.substring(0,4))-(7+sub))+formattedDate.substring(4));
+        getCard(imageView8, textView8, ""+(Integer.parseInt(formattedDate.substring(0,4))-(8+sub))+formattedDate.substring(4));
+        getCard(imageView9, textView9, ""+(Integer.parseInt(formattedDate.substring(0,4))-(9+sub))+formattedDate.substring(4));
+        getCard(imageView10, textView10, ""+(Integer.parseInt(formattedDate.substring(0,4))-(10+sub))+formattedDate.substring(4));
     }
 
     public void getCard(ImageView imageView, TextView textView, String date){
@@ -67,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         JSONObject jsonObject = jsonParser.getJSONFromUrl("https://api.nasa.gov/planetary/apod?api_key=uZvWdb4HL99nymXFPQlFfUBVZSuumS0MvqlVUHXs&date="+date, null);
         String url = "https://fabiusmaximus.files.wordpress.com/2012/12/20121230-no-error.png", text="Title not found";
         try {
-            url = jsonObject.get("hdurl").toString();
+            url = jsonObject.get("url").toString();
             text = jsonObject.get("title").toString();
         } catch (JSONException e) {
             e.printStackTrace();
