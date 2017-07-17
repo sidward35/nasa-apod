c77d587c452bfc93f901df9690cbbf867e01e3a2
Siddharth Mathur <sid@stscamps.org>
2017-07-16 17:53:02 -0700

    View one image at a time

--- app/src/main/java/com/sid/nasaapod/MainActivity.java
+++ app/src/main/java/com/sid/nasaapod/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        final ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
+        //final ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
         //final int[] subtraction = {0};
         updateImages(/*subtraction[0]*/);
         Button button = (Button)findViewById(R.id.button);
@@ -52,29 +52,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 //subtraction[0] +=10;
                 updateImages(/*subtraction[0]*/);
-                scrollView.fullScroll(ScrollView.FOCUS_UP);
+                //scrollView.fullScroll(ScrollView.FOCUS_UP);
             }
         });
     }
 
     public void updateImages(){
-        ImageView imageView = (ImageView)findViewById(R.id.imageView), imageView2 = (ImageView)findViewById(R.id.imageView2), imageView3 = (ImageView)findViewById(R.id.imageView3), imageView4 = (ImageView)findViewById(R.id.imageView4), imageView5 = (ImageView)findViewById(R.id.imageView5), imageView6 = (ImageView)findViewById(R.id.imageView6), imageView7 = (ImageView)findViewById(R.id.imageView7), imageView8 = (ImageView)findViewById(R.id.imageView8), imageView9 = (ImageView)findViewById(R.id.imageView9), imageView10 = (ImageView)findViewById(R.id.imageView10);
-        TextView textView = (TextView)findViewById(R.id.textView), textView2 = (TextView)findViewById(R.id.textView2), textView3 = (TextView)findViewById(R.id.textView3), textView4 = (TextView)findViewById(R.id.textView4), textView5 = (TextView)findViewById(R.id.textView5), textView6 = (TextView)findViewById(R.id.textView6), textView7 = (TextView)findViewById(R.id.textView7), textView8 = (TextView)findViewById(R.id.textView8), textView9 = (TextView)findViewById(R.id.textView9), textView10 = (TextView)findViewById(R.id.textView10);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);//, imageView2 = (ImageView)findViewById(R.id.imageView2), imageView3 = (ImageView)findViewById(R.id.imageView3), imageView4 = (ImageView)findViewById(R.id.imageView4), imageView5 = (ImageView)findViewById(R.id.imageView5), imageView6 = (ImageView)findViewById(R.id.imageView6), imageView7 = (ImageView)findViewById(R.id.imageView7), imageView8 = (ImageView)findViewById(R.id.imageView8), imageView9 = (ImageView)findViewById(R.id.imageView9), imageView10 = (ImageView)findViewById(R.id.imageView10);
+        TextView textView = (TextView)findViewById(R.id.textView);//, textView2 = (TextView)findViewById(R.id.textView2), textView3 = (TextView)findViewById(R.id.textView3), textView4 = (TextView)findViewById(R.id.textView4), textView5 = (TextView)findViewById(R.id.textView5), textView6 = (TextView)findViewById(R.id.textView6), textView7 = (TextView)findViewById(R.id.textView7), textView8 = (TextView)findViewById(R.id.textView8), textView9 = (TextView)findViewById(R.id.textView9), textView10 = (TextView)findViewById(R.id.textView10);
 
         getCard(imageView, textView, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView2, textView2, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView3, textView3, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView4, textView4, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView5, textView5, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView6, textView6, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView7, textView7, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView8, textView8, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView9, textView9, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
-        getCard(imageView10, textView10, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView2, textView2, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView3, textView3, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView4, textView4, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView5, textView5, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView6, textView6, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView7, textView7, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView8, textView8, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView9, textView9, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
+        //getCard(imageView10, textView10, ""+((int)(Math.random()*21+1996))+"-"+((int)(Math.random()*12+1))+"-"+((int)(Math.random()*28+1)));
     }
 
 
-    public void updateImages(int sub){
+    /*public void updateImages(int sub){
         ImageView imageView = (ImageView)findViewById(R.id.imageView), imageView2 = (ImageView)findViewById(R.id.imageView2), imageView3 = (ImageView)findViewById(R.id.imageView3), imageView4 = (ImageView)findViewById(R.id.imageView4), imageView5 = (ImageView)findViewById(R.id.imageView5), imageView6 = (ImageView)findViewById(R.id.imageView6), imageView7 = (ImageView)findViewById(R.id.imageView7), imageView8 = (ImageView)findViewById(R.id.imageView8), imageView9 = (ImageView)findViewById(R.id.imageView9), imageView10 = (ImageView)findViewById(R.id.imageView10);
         TextView textView = (TextView)findViewById(R.id.textView), textView2 = (TextView)findViewById(R.id.textView2), textView3 = (TextView)findViewById(R.id.textView3), textView4 = (TextView)findViewById(R.id.textView4), textView5 = (TextView)findViewById(R.id.textView5), textView6 = (TextView)findViewById(R.id.textView6), textView7 = (TextView)findViewById(R.id.textView7), textView8 = (TextView)findViewById(R.id.textView8), textView9 = (TextView)findViewById(R.id.textView9), textView10 = (TextView)findViewById(R.id.textView10);
 
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         getCard(imageView8, textView8, ""+(Integer.parseInt(formattedDate.substring(0,4))-(8+sub))+formattedDate.substring(4));
         getCard(imageView9, textView9, ""+(Integer.parseInt(formattedDate.substring(0,4))-(9+sub))+formattedDate.substring(4));
         getCard(imageView10, textView10, ""+(Integer.parseInt(formattedDate.substring(0,4))-(10+sub))+formattedDate.substring(4));
-    }
+    }*/
 
     public void getCard(ImageView imageView, TextView textView, String date){
         JSONParser jsonParser = new JSONParser();
